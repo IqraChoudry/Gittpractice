8a01781de423eff38684f149733922f30df964b1
Iqra <iqrachoudry92@gmail.com>
2022-03-22 03:17:46 -0400

    I am commiting this message

--- src/gitPackage/GitPractice.java
+++ src/gitPackage/GitPractice.java
@@ -4,7 +4,7 @@ public class GitPractice {
 
 	public static void main(String[] args) {
  
-		System.out.println("My Name is Talentech");
+		System.out.println("My Name is Talentechhh");
 		System.out.println(" WHat is your name???");
 	}
 
