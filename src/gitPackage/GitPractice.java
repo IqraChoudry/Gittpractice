e652a6b8973617fd04fed171e7bc1795c6ce20cd
Iqra <iqrachoudry92@gmail.com>
2022-03-22 02:53:11 -0400

    Second Commit

---
+++ src/gitPackage/GitPractice.java
@@ -0,0 +1,11 @@
+package gitPackage;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+ 
+		System.out.println("My Name is Talentech");
+		System.out.println(" WHat is your name??");
+	}
+
+}
